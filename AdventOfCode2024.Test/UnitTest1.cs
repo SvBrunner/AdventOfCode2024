11da5712fc8548f42192057318e81d3be3ecc2c6
SvBrunner <sven.brunner@ewmmail.ch>
2024-11-30 11:48:30 +0100

    :tada: init

---
+++ AdventOfCode2024.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode2024.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
